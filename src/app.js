35b5c4678061f262e8fc9b59e8a05daca6282fe7
Hristo <htsurev@gmail.com>
2024-09-10 13:08:32 +0100

    update tooling configs

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-import page from "@page/page.mjs";
+import page from '@page/page.mjs';
 import { classMap } from "@lit/directives/class-map.js";
 import { styleMap } from "@lit/directives/style-map.js";
 import { repeat } from "@lit/directives/repeat.js";
 import { until } from "@lit/directives/until.js";
 
-import { showBrowseView } from "./views/browseView.js";
-import { showLoginView } from "./views/loginView.js";
-import { showProfileView } from "./views/profileView.js";
-import { showRegisterView } from "./views/registerView.js";
-import { showWelcomeView } from "./views/welcomeView.js";
+import { showBrowseView } from './views/browseView.js';
+import { showLoginView } from './views/loginView.js';
+import { showProfileView } from './views/profileView.js';
+import { showRegisterView } from './views/registerView.js';
+import { showWelcomeView } from './views/welcomeView.js';
 
 
-document.getElementById("user-nav").style.display = "block";
-document.getElementById("guest-nav").style.display = "block";
+document.getElementById('user-nav').style.display = 'block';
+document.getElementById('guest-nav').style.display = 'block';
 
 
-page("/", showWelcomeView);
-page("/login", showLoginView);
-page("/register", showRegisterView);
-page("/browse", showBrowseView);
-page("/profile", showProfileView);
+page('/', showWelcomeView);
+page('/login', showLoginView);
+page('/register', showRegisterView);
+page('/browse', showBrowseView);
+page('/profile', showProfileView);
 
-page.start()
+page.start();
 
